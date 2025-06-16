fa225db1e9e2982667adaf02b031930065e81b9e
Kareem-Mohamed-Wardany <kareemwardany1111@gmail.com>
2025-06-16 22:49:40 +0300

    Added missing prop

--- MovieAPI/DTO/DirectorMovieToReturnDTO.cs
+++ MovieAPI/DTO/DirectorMovieToReturnDTO.cs
@@ -5,6 +5,7 @@ namespace MovieAPI.DTO
     public class DirectorMovieToReturnDTO
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public MovieGenre Genre { get; set; }
         public string Duration { get; set; }
